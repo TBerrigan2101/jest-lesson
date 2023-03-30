59f65df914798f8bf6b86b79a51be15d86ea7b13
TBerrigan2101 <tberrigan2101@gmail.com>
2023-03-30 21:00:36 +0000

    Second commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,8 +1,27 @@
 let game = {
-    score: 0,
-    currentGame: [], 
+    currentGame: [],
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"], 
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
-module.exports = { game };
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+module.exports = { game, newGame, showScore, addTurn };
+
