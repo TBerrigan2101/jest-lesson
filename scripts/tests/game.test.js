5bab05c38686a67bd2ad1a94bf73407b79c6e9f1
TBerrigan2101 <tberrigan2101@gmail.com>
2023-04-02 22:25:37 +0000

    Added additional functions and tests

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,8 +2,15 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8")
+    document.open();
+    document.write(fileContents);
+    document.cloneNode();
+});
 
 beforeAll(() => {
     let fs = require("fs");
@@ -29,6 +36,9 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turnNumber key exist", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
 });
 
 describe("newGame works correctly", () => {
@@ -50,6 +60,12 @@ describe("newGame works correctly", () => {
     });
     test("should be one move in the computer's game array", () => {
         expect(game.currentGame.length).toBe(1);
+    });
+    test("expect date-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
     });
 });
 
@@ -73,5 +89,10 @@ describe("gameplay works correctly", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 43;
+        showTurns();
+        expect(game.turnNumber).toBe(0); 
     });
 });
